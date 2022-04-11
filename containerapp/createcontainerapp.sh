330baf6d92d626d1f5535c2134a982315b90c962
Daniel Tovar <danieldaniyyelda@gmail.com>
2022-04-10 23:05:23 -0400

    add image name
    Signed-off-by: Daniel Tovar <danieldaniyyelda@gmail.com>

--- containerapp/createcontainerapp.sh
+++ containerapp/createcontainerapp.sh
@@ -1,23 +1,35 @@
 source vars.sh
 
-##CREATE RESOURCE GROUP
-az group create \
-  --name $RESOURCE_GROUP \
-  --location $LOCATION
+# ##CREATE RESOURCE GROUP
+# az group create \
+#   --name $RESOURCE_GROUP \
+#   --location $LOCATION
 
-##CREATE ENV 
-az containerapp env create \
-  --name $CONTAINERAPPS_ENVIRONMENT \
-  --resource-group $RESOURCE_GROUP \
-  --location $LOCATION
+# ##CREATE ENV 
+# az containerapp env create \
+#   --name $CONTAINERAPPS_ENVIRONMENT \
+#   --resource-group $RESOURCE_GROUP \
+#   --location $LOCATION
 
-## CREATE APP
+# ## CREATE AUTH APP
+# az containerapp create -n $CONTAINERAPP_NAME -g $RESOURCE_GROUP \
+#     --image $IMAGE --environment $CONTAINERAPPS_ENVIRONMENT \
+#     --ingress external --target-port 80 \
+#     --registry-server $REGISTRY_SERVER --registry-username $REGISTRY_USER --registry-password $REGISTRY_PASSWORD \
+#     --query properties.configuration.ingress.fqdn \
+#     --secrets defaultconnection=$DBCON sendgridkey=$SENDGRIDKEY \
+#     --env-vars ConnectionStrings__DefaultConnection=secretref:defaultconnection SendGridKey=secretref:sendgridkey \
+#     --cpu 0.5 --memory 1.0Gi \
+#     --min-replicas 0 --max-replicas 5
+
+
+## CREATE SERVICES APP
 az containerapp create -n $CONTAINERAPP_NAME -g $RESOURCE_GROUP \
     --image $IMAGE --environment $CONTAINERAPPS_ENVIRONMENT \
     --ingress external --target-port 80 \
     --registry-server $REGISTRY_SERVER --registry-username $REGISTRY_USER --registry-password $REGISTRY_PASSWORD \
     --query properties.configuration.ingress.fqdn \
-    --secrets defaultconnection=$DBCON sendgridkey=$SENDGRIDKEY \
-    --env-vars ConnectionStrings__DefaultConnection=secretref:defaultconnection SendGridKey=secretref:sendgridkey \
+    --secrets defaultconnection=$DBCON \
+    --env-vars ConnectionStrings__DefaultConnection=secretref:defaultconnection \
     --cpu 0.5 --memory 1.0Gi \
     --min-replicas 0 --max-replicas 5
